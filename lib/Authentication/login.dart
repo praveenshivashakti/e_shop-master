a1e099a7c4bc19d19d029c500111b46827646b09
praveen dibbi <ptest5557@gmail.com>
2020-10-03 02:07:01 +0530

    auth

--- lib/Authentication/login.dart
+++ lib/Authentication/login.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../Store/storehome.dart';
 import 'package:e_shop/Config/config.dart';
+import 'package:e_shop/animation/FadeAnimation.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -13,20 +14,155 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
                 height: 400,
-                child: Image(
-                  image: AssetImage("images/login.jpg"),
-                  fit: BoxFit.contain,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      top: -40,
+                      height: 400,
+                      width: width * 1,
+                      child: FadeAnimation(
+                        1,
+                        Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/login_cano.png'),
+                                  fit: BoxFit.fill)),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      height: 400,
+                      width: width + 20,
+                      child: FadeAnimation(
+                          1.3,
+                          Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage('assets/background-2.png'),
+                                    fit: BoxFit.fill)),
+                          )),
+                    )
+                  ],
                 ),
               ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    FadeAnimation(
+                        1.5,
+                        Text(
+                          "Register",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 30),
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FadeAnimation(
+                        1.7,
+                        Form(
+                          key: _formKey,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.purple[50],
+                                  blurRadius: 25.0, // soften the shadow
+                                  spreadRadius: 5.0, //extend the shadow
+                                  offset: Offset(
+                                    10.0, // Move to right 10  horizontally
+                                    1.0, // Move to bottom 10 Vertically
+                                  ),
+                                )
+                              ],
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                CustomTextField(
+                                    controller: _emailTextEditingController,
+                                    data: Icons.person,
+                                    hintText: "Email",
+                                    isObsecure: false),
+                                CustomTextField(
+                                    controller: _passwordTextEditingController,
+                                    data: Icons.person,
+                                    hintText: "password",
+                                    isObsecure: true),
+                              ],
+                            ),
+                          ),
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FadeAnimation(
+                        1.9,
+                        Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 60),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Center(
+                            child: MaterialButton(
+                              minWidth: double.infinity,
+                              height: 60,
+                              color: Colors.purple[200],
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              onPressed: () {},
+                              child: Text(
+                                "Sign up",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        )),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    FadeAnimation(
+                        2,
+                        Center(
+                            child: Text(
+                          "Create Account",
+                        ))),
+                  ],
+                ),
+              )
             ],
           ),
         ),
@@ -34,32 +170,3 @@ class _LoginState extends State<Login> {
     );
   }
 }
-
-// class LoginPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Container(
-//           width: double.infinity,
-//           height: MediaQuery.of(context).size.height,
-//           child: Column(
-//             children: <Widget>[
-//               Column(
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: <Widget>[
-//                   Text("Login"),
-//                 ],
-//               ),
-//               Container(
-//                   height: MediaQuery.of(context).size.height / 3,
-//                   decoration: BoxDecoration(
-//                       image: DecorationImage(
-//                           image: AssetImage('assets/Illustration.png'))))
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
